55b1123eb57a376a857f97de62ce066de4f1d36d
Tobias-Santos <117104950+Tobias-Santos@users.noreply.github.com>
2025-05-15 22:27:10 -0300

    Consertando bugs

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import sys
 import os
-
+import json
+import multiprocessing
+from collections import defaultdict
+import redis
 
 sys.path.append(os.path.abspath('./utils'))
 
 from utils import mapper, shuffle, reducer
-import multiprocessing
-import json
-from collections import defaultdict
+
+# Configuração do Redis
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 def verify_chunks(chunk_dir, num_chunks):
     """Verifica se todos os arquivos chunkX.txt existem na pasta chunks."""
@@ -24,22 +27,18 @@ def run_mapper(chunks):
     with multiprocessing.Pool(processes=len(chunks)) as pool:
         pool.map(mapper.mapper, chunks)
 
-def combine_results(output_dir='./output', result_file='./result.txt'):
-    """Combina os resultados dos arquivos reduced_X_output.json e gera result.txt."""
-    if not os.path.isdir(output_dir):
-        print("A pasta 'output' não existe ou está vazia.")
-        return
-
+def combine_results(result_file='./result.txt'):
+    """Combina os resultados armazenados no Redis e gera result.txt."""
     combined_counts = defaultdict(int)
 
+    # Recupera os resultados do Redis
+    keys = redis_client.keys('reduce_*')
+    for key in keys:
+        data = json.loads(redis_client.get(key))
+        for word, counts in data.items():
+            combined_counts[word] += sum(counts)
 
-    for file in os.listdir(output_dir):
-        if file.endswith('_output.json'):
-            with open(os.path.join(output_dir, file), 'r', encoding='utf-8') as f:
-                data = json.load(f)
-                for word, count in data.items():
-                    combined_counts[word] += count
-
+    # Salva os resultados combinados em um arquivo
     with open(result_file, 'w', encoding='utf-8') as f:
         for word, total_count in combined_counts.items():
             f.write(f"{word}:{total_count}\n")
@@ -51,20 +50,21 @@ def main():
     chunk_dir = './chunks'
     num_chunks = 10
 
-
+    # Verifica os arquivos de entrada
     chunks = verify_chunks(chunk_dir, num_chunks)
 
- 
+    # Executa o mapper
     run_mapper(chunks)
 
-   
+    # Executa o shuffle
     print("Executando o shuffle...")
-    shuffle.shuffle()
+    shuffle.shuffle(redis_client)
 
+    # Executa o reducer
     print("Executando o reducer...")
-    reducer.reducer()
+    reducer.reducer(redis_client)
 
-    
+    # Combina os resultados
     print("Combinando os resultados...")
     combine_results()
 
